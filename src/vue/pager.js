3be725cb045f0d4fdc541b52087fbcbd422b5250
Martin Guillon <martin.guillon@akylas.fr>
2020-10-21 17:44:58 +0200

    fix vue

--- src/vue/pager.js
+++ src/vue/pager.js
@@ -28,61 +28,89 @@ module.exports = function pager(Vue) {
       ref="pagerView"
       :items="items"
 	  v-bind="$attrs"
+	  v-on="listeners"
       :selectedIndex="selectedIndex"
 	  @itemLoading="onItemLoading"
 	  @itemDisposing="onItemDisposing">
       <slot />
     </NativePager>
   `,
-		watch: {
-			items: {
-				handler(newVal) {
-					this.$refs.pagerView.setAttribute('items', newVal);
-					this.$refs.pagerView.nativeView.refresh();
-				},
-				deep: true
-			}
-		},
-		mounted() {
-			if (!this.items) return;
-			this.getItemContext = (item, index) =>
-				getItemContext(item, index, this.$props[ '+alias' ], this.$props[ '+index' ]);
-			this.$refs.pagerView.setAttribute('items', this.items);
-			this.$refs.pagerView.setAttribute(
-				'_itemTemplatesInternal',
-				this.$templates.getKeyedTemplates()
-			);
-			this.$refs.pagerView.setAttribute(
-				'_itemTemplateSelector',
-				(item, index) => {
-					return this.$templates.selectorFn(this.getItemContext(item, index));
-				}
-			);
-		},
-		methods: {
-			onItemLoading(args) {
-				if (!this.items) return;
-				const index = args.index;
-				const items = args.object.items;
-				const currentItem =
-					typeof items.getItem === 'function'
-						? items.getItem(index)
-						: items[ index ];
-				const name = args.object._itemTemplateSelector(currentItem, index, items);
-				const context = this.getItemContext(currentItem, index);
-				const oldVnode = args.view && args.view[ VUE_VIEW ];
-				args.view = this.$templates.patchTemplate(name, context, oldVnode);
-			},
-			onItemDisposing(args) {
-				// TODO: handle disposing template
-				// const oldVnode = args.view && args.view[ VUE_VIEW ];
-				// console.log("disposing", !!oldVnode, VUE_VIEW);
-				// if (oldVnode) {
-				// 	Vue.prototype.__patch__(oldVnode, null);
-				// }
-			}
-		}
-	};
+        watch: {
+            items: {
+                handler(newVal) {
+                    this.$refs.pagerView.setAttribute("items", newVal);
+                    this.$refs.pagerView.nativeView.refresh();
+                },
+                deep: true,
+            },
+        },
+        computed: {
+            listeners() {
+                return Object.assign({}, this.$listeners, {
+                    selectedIndexChange: this.onSelectedIndexChange,
+                });
+            },
+        },
+        mounted() {
+            if (!this.items) return;
+            this.getItemContext = (item, index) =>
+                getItemContext(
+                    item,
+                    index,
+                    this.$props["+alias"],
+                    this.$props["+index"]
+                );
+            this.$refs.pagerView.setAttribute("items", this.items);
+            this.$refs.pagerView.setAttribute(
+                "_itemTemplatesInternal",
+                this.$templates.getKeyedTemplates()
+            );
+            this.$refs.pagerView.setAttribute(
+                "_itemTemplateSelector",
+                (item, index) => {
+                    return this.$templates.selectorFn(
+                        this.getItemContext(item, index)
+                    );
+                }
+            );
+        },
+        methods: {
+            onItemLoading(args) {
+                if (!this.items) return;
+                const index = args.index;
+                const items = args.object.items;
+                const currentItem =
+                    typeof items.getItem === "function"
+                        ? items.getItem(index)
+                        : items[index];
+                const name = args.object._itemTemplateSelector(
+                    currentItem,
+                    index,
+                    items
+                );
+                const context = this.getItemContext(currentItem, index);
+                const oldVnode = args.view && args.view[VUE_VIEW];
+                args.view = this.$templates.patchTemplate(
+                    name,
+                    context,
+                    oldVnode
+                );
+            },
+            onItemDisposing(args) {
+                // TODO: handle disposing template
+                // const oldVnode = args.view && args.view[ VUE_VIEW ];
+                // console.log("disposing", !!oldVnode, VUE_VIEW);
+                // if (oldVnode) {
+                // 	Vue.prototype.__patch__(oldVnode, null);
+                // }
+            },
+            onSelectedIndexChange({ value }) {
+                this.$emit("selectedIndexChange", {
+                    object: { selectedIndex: value },
+                });
+            },
+        },
+    };
 
     function getItemContext(item, index, alias, index_alias) {
         return {
