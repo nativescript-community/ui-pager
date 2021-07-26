c543697f6fd6e3e2504827bd8fb4faac3a292e81
Martin Guillon <martin.guillon@akylas.fr>
2021-07-26 10:37:17 +0200

    fix: vue fix

--- src/vue/index.ts
+++ src/vue/index.ts
@@ -2,7 +2,7 @@ const Plugin = {
     install(Vue) {
         Vue.registerElement('NativePager', () => require('../').Pager);
         Vue.registerElement('PagerItem', () => require('../').PagerItem);
-        Vue.component('Pager', require('./pager')(Vue));
+        Vue.component('Pager', require('./pager').default);
     }
 };
 
