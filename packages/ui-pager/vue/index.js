09d829855f0cdab8c39f3fd47a80fcb4b0ba5b45
Karl Schmaltz <karlschmaltz@gmail.com>
2021-10-29 13:53:57 -0600

    chore: remove files that should be ignored

--- packages/ui-pager/vue/index.js
+++
@@ -1,9 +0,0 @@
-const Plugin = {
-    install(Vue) {
-        Vue.registerElement('NativePager', () => require('../').Pager);
-        Vue.registerElement('PagerItem', () => require('../').PagerItem);
-        Vue.component('Pager', require('./pager').default);
-    }
-};
-export default Plugin;
-//# sourceMappingURL=index.js.map
