e7a8515898f7dc4f40f520d8874270df2a8aeb06
Martin Guillon <martin.guillon@akylas.fr>
2021-11-17 16:10:21 +0100

    test: demo updates

--- demo-snippets/svelte/StaticPager.svelte
+++ demo-snippets/svelte/StaticPager.svelte
@@ -1,29 +1,27 @@
-<style>
-
-.label {
-    font-size: 35;
-    horizontal-alignment: center;
-    vertical-alignment: center;
-    color: #FFFFFF;
-    text-transform: uppercase;
-}
-
-</style>
+<script lang="typescript">
+    import { goBack } from 'svelte-native';
+</script>
 
 <page>
     <actionBar title="Static Pager">
-        <navigationButton text="Go back" on:tap={goBack} />
+        <navigationButton text="Go back" on:tap={() => goBack()} />
     </actionBar>
-    <stackLayout class="page">
-        <pager height="100%">
+    <gridlayout class="page">
+        <pager>
             <pageritem backgroundColor="#e67e22"><label class="label" text="First" /></pageritem>
             <pageritem backgroundColor="#3498db"><label class="label" text="Second" /></pageritem>
             <pageritem backgroundColor="#e74c3c"><label class="label" text="Third" /></pageritem>
             <pageritem backgroundColor="#9b59b6"><label class="label" text="Fourth" /></pageritem>
         </pager>
-    </stackLayout>
+    </gridlayout>
 </page>
 
-<script lang="typescript">
-    import { goBack } from 'svelte-native';
-</script>
+<style>
+    .label {
+        font-size: 35;
+        horizontal-alignment: center;
+        vertical-alignment: center;
+        color: #ffffff;
+        text-transform: uppercase;
+    }
+</style>
