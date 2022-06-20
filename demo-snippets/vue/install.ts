54f6e9a6eaa7bdfda322e1ecb36c0170d484b779
Karl Schmaltz <karlschmaltz@gmail.com>
2022-06-20 06:42:45 -0600

    chore: add bug example

--- demo-snippets/vue/install.ts
+++ demo-snippets/vue/install.ts
@@ -4,6 +4,7 @@ import Pager from '@nativescript-community/ui-pager/vue';
 import StaticPager from './StaticPager.vue';
 import BasicPager from './BasicPager.vue';
 import Indicator from './Indicator.vue';
+import RenderIssue from './RenderIssue.vue';
 
 export function installPlugin() {
     Vue.use(Pager);
@@ -12,5 +13,6 @@ export function installPlugin() {
 export const demos = [
     { name: 'Static Pager', path: 'static', component: StaticPager },
     { name: 'Basic Pager', path: 'basic', component: BasicPager },
-    { name: 'Indicator', path: 'indicator', component: Indicator }
+    { name: 'Indicator', path: 'indicator', component: Indicator },
+    { name: 'Render Issue', path: 'indicator', component: RenderIssue }
 ];
