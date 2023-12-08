cebcd6d959b17f741ab4f152f71bec5a8c430fa9
farfromrefuge <>
2023-12-08 17:08:41 +0100

    chore: tsc

--- src/ui-pager/svelte/index.ts
+++ src/ui-pager/svelte/index.ts
@@ -2,6 +2,7 @@ import { ItemEventData, View } from '@nativescript/core';
 import { profile } from '@nativescript/core/profiling';
 import { ContentView, LayoutBase, StackLayout, ViewBase } from '@nativescript/core/ui';
 import { NativeViewElementNode, TemplateElement, ViewNode, createElement, registerElement, registerNativeViewElement } from 'svelte-native/dom';
+//@ts-ignore
 import { flush } from 'svelte/internal';
 import { Pager, PagerItem } from '..';
 
